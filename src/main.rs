12203ef309f928814d6d605468c56dadb7837063
weilycoder <weily2009@outlook.com>
2025-10-03 21:39:08 +0800

    feat: Add initial build script and LALRPOP grammar for sysy-compiler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use lalrpop_util::lalrpop_mod;
+use std::env::args;
+use std::fs::read_to_string;
+use std::io::Result;
+
+lalrpop_mod!(sysy);
+
+fn main() -> Result<()> {
+    let mut args = args();
+    args.next(); // skip executable name
+    let input = args.next().unwrap();
+    let output = args.next().unwrap();
+
+    let input = read_to_string(input)?;
+
+    let ast = sysy::CompUnitParser::new().parse(&input).unwrap();
+
+    std::fs::write(output, ast.as_bytes())?;
+    Ok(())
 }
